6647e0300e99c8a23b2348d4a1a4d667a31d99a9
seojuhui <149547601+JullyJuhui@users.noreply.github.com>
2026-04-15 18:37:08 +0900

    feat: base_g 계산 중

---
+++ ai/test/test_base_formula.py
@@ -0,0 +1,19 @@
+from ai.recommend.base_formula.food_amount_cal_add_size import calculate_base_g
+
+# weight = 7.5
+# age_m = 13 # 개월수
+# neutered = True
+# BCS = 6
+# adult_month = 13
+# food_kcal = 4.005
+
+base_g, goal_weight = calculate_base_g(
+        weight=7.5, 
+        age_m=14, 
+        neutered=True, 
+        bcs=6, 
+        adult_month=13, 
+        food_kcal=4.005
+    )
+
+print(base_g)

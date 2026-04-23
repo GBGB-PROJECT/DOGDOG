00ad9dcb8cdac7449bf059b5d3f185e45e6567f0
junhwan <junhwan1310@gmail.com>
2026-04-23 18:58:46 +0900

    FEAT: 준환 건형의 MAIN 실행을 위한 DB 복사

---
+++ db/db.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# DB URL
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    # connect_args={"options": "-csearch_path=public,dog_5"},
+    echo=True,
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

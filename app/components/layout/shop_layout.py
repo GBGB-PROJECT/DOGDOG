dd84cd275aca1eb5f4a5d3e2652b27497596d169
Ru_ <parkjaegun0219@gmail.com>
2026-04-28 18:54:55 +0900

    update

--- app/components/layout/shop_layout.py
+++ app/components/layout/shop_layout.py
@@ -2,15 +2,20 @@ import flet as ft
 import components as dogdog
 
 def product(page, p_id, image_src, image_size, title, price):
+    image = ft.Container(width=image_size, height=image_size, image=ft.DecorationImage(src=image_src))
+    product_name = dogdog.basic_text(value=title, size=12, color=ft.Colors.GREY_800)
+    product_name.max_lines = 1
+    product_name.overflow = ft.TextOverflow.ELLIPSIS
+    product_name.width = image_size
+    product_name.text_align = ft.TextAlign.CENTER
+    product_price = dogdog.basic_text(value=price, size=12, color=ft.Colors.GREY_800)
     return ft.Container(
         padding=0,
-        on_click=lambda _:page.go(f"/shop/detail_{p_id}"),
+        on_click=lambda _:page.go(f"/shop/product/{p_id}"),
         ink=True,
-        content=ft.Column(spacing=0, horizontal_alignment=ft.CrossAxisAlignment.CENTER, controls=[
-            ft.Container(width=image_size, height=image_size, image=ft.DecorationImage(src=image_src)),
-            dogdog.basic_text(value=title, size=12, color=ft.Colors.GREY_800),
-            dogdog.basic_text(value=price, size=12, color=ft.Colors.GREY_800)
-    ]))
+        content=ft.Column(spacing=0, horizontal_alignment=ft.CrossAxisAlignment.CENTER, 
+            controls=[image, product_name, product_price]
+    ))
 
 def products(page, data_dict, image_size):
     items = list(data_dict.items())
@@ -20,7 +25,13 @@ def products(page, data_dict, image_size):
         row_controls = []
         for p_id, p_d in chunk:
             row_controls.append(
-                product(page, p_id, p_d["image_src"], image_size, p_d["product_name"], f"{p_d['sales_price']}원")
+                product(page, 
+                    p_id, 
+                    p_d["thumbnail"], 
+                    image_size, 
+                    f"{p_d["brand"]} {p_d["product_name"]}", 
+                    f"{int(p_d['sales_price']):,}원"
+                )
             )
         products.append(
             ft.Row(

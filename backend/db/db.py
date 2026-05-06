f8fc7e8e78082a805cc989d9f9adf05e0055229d
Kims-club98 <naxen5611@naver.com>
2026-05-06 16:22:05 +0900

    feat: models, db 최신화

--- backend/db/db.py
+++ backend/db/db.py
@@ -10,8 +10,9 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
+    pool_pre_ping=True,  # [안정성] 연결을 풀에서 꺼낼 때마다 유효성 검사 수행 (서버 예기치 않은 종료 에러 방지)
     # connect_args={"options": "-csearch_path=public,dog_5"},
-    echo=os.getenv("SQLALCHEMY_ECHO", "").lower() in {"1", "true", "yes", "on"},
+    echo=True,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
